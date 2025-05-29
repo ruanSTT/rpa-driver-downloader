da04b58d22f5e1f827dc0c69bee99be64bc6f672
Ruan Machado <whiterun092@gmail.com>
2025-05-29 00:40:37 -0300

    feat: setup.py update

--- setup.py
+++ setup.py
@@ -5,25 +5,31 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="rpa-driver-downloader",
-    version="0.1.1",
+    version="0.2.0",
     author="Ruan Chaves Machado",
     author_email="whiterun092@gmail.com",
     description="A Python package to automatically download and manage WebDrivers for Gecko, Chrome, and Edge.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/your-username/rpa-driver-downloader",
+    url="https://github.com/ruanSTT/rpa-driver-downloader",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries",
         "Topic :: Utilities",
     ],
-    python_requires='>=3.7, <3.12',
+    python_requires='>=3.7, <=3.12.3',
     install_requires=[
-        "requests",
+        "requests>=2.31.0"
     ],
     include_package_data=True,
 )
