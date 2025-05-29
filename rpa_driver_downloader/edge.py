a3247876dbc29db378fcb3ebbbebe720134b349a
Ruan Machado <whiterun092@gmail.com>
2025-05-29 00:41:43 -0300

    feat: edge.py update

--- rpa_driver_downloader/edge.py
+++ rpa_driver_downloader/edge.py
@@ -1,22 +1,40 @@
 from .base import BaseDriver
 import platform
+import requests
 
 
 class Edge(BaseDriver):
+    def __init__(self, version: str = None):
+        self.version = version or "124.0.2478.0"  # Default fallback version
+        super().__init__(version=self.version)
+
+
     @property
-    def key_name(self):
+    def key_name(self) -> str:
         return "edgedriver_path"
+
+
+    def get_latest_version(self) -> str:
+        return self.version
 
 
     def get_download_url(self) -> str:
         os_name = platform.system()
+        url = f"https://msedgedriver.azureedge.net/{self.version}/edgedriver_"
 
-        version = "124.0.2478.0"
         if os_name == "Windows":
-            return f"https://msedgedriver.azureedge.net/{version}/edgedriver_win64.zip"
+            url += "win64.zip"
         elif os_name == "Linux":
-            return f"https://msedgedriver.azureedge.net/{version}/edgedriver_linux64.zip"
+            url += "linux64.zip"
         elif os_name == "Darwin":
-            return f"https://msedgedriver.azureedge.net/{version}/edgedriver_mac64.zip"
+            url += "mac64.zip"
+        else:
+            raise FileNotFoundError("Edge driver download URL not found.")
 
-        raise FileNotFoundError("Driver Edge not found.")
+        resp = requests.get(url, stream=True)
+        if resp.status_code != 200:
+            raise ValueError(
+                f"❌ EdgeDriver version '{self.version}' not found.\n"
+                f"🔗 See all versions at: https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/"
+            )
+        return url
